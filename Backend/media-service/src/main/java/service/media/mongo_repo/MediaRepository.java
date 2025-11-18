a36c47034dc8daf1a5aa64c7594d75df82aadfc0
ToniKorhonen <toni.korhonenmatias@gmail.com>
2025-11-18 09:00:59 +0100

    mongo repo added

---
+++ Backend/media-service/src/main/java/service/media/mongo_repo/MediaRepository.java
@@ -0,0 +1,11 @@
+package service.media.mongo_repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import service.media.models.Media;
+
+import java.util.List;
+
+public interface MediaRepository extends MongoRepository<Media, String> {
+    List<Media> findByProductId(String productId);
+    List<Media> findByUploaderId(String uploaderId);
+}
