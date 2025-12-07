5e2c7f02315a72e9d65d4b057830647506c7f96e
ToniKorhonen <toni.korhonenmatias@gmail.com>
2025-12-07 20:49:14 +0100

    Sonarqube local analysis functional

--- Backend/user-service/src/test/java/service/user/ServiceUserApplicationTests.java
+++ Backend/user-service/src/test/java/service/user/ServiceUserApplicationTests.java
@@ -2,8 +2,10 @@ package service.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class ServiceUserApplicationTests {
 
 	@Test
