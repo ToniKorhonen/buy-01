1c48d107fb7bf549a876b9505b6103cf3d1ae0ff
ToniKorhonen <toni.korhonenmatias@gmail.com>
2026-01-13 13:07:24 +0100

    updated status enum

--- Backend/order-service/src/main/java/service/order/models/Status.java
+++ Backend/order-service/src/main/java/service/order/models/Status.java
@@ -1,6 +1,7 @@
 package service.order.models;
 
 public enum Status {
+    ADDED, // when item is added to cart
     STARTED, // initial status when order is created
     ONGOING, // when the order is being processed( started by seller)
     RECEIVED, // when the buyer has received the order(set by buyer)
